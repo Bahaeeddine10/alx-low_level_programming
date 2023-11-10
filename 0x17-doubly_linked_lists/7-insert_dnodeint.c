5a61f970add2d3bdb75432d62a6bf711e20bee77
“bahaeeddine10” <bahaeeddine10@gmail.com>
2023-11-10 10:56:50 -0800

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,38 +1,55 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at
- * a given position
+ * insert_dnodeint_at_index - inserts a new node in dlistint_t
+ *                             list at a given position
  *
  * @h: head of the list
  * @idx: index of the new node
  * @n: value of the new node
+ 
  * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp =*h, *new;
+  dlistint_t *new;
+  dlistint_t *head;
+  unsigned int i;
 
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	for (idx != 1, idx--)
-	{
-		temp = temp->next;
-		if (temp == NULL)
-			return (NULL);
-	}
-	if (temp->next == NULL)
-		return (add_dnodeint_end(h, n));
+  new = NULL;
+  if (idx == 0)
+    new = add_dnodeint(h, n);
+  else
+  {
+    head = *h;
+    i = 1;
+    if (head != NULL)
+      while (head->prev != NULL)
+        head = head->prev;
+    while (head != NULL)
+    {
+      if (i == idx)
+      {
+        if (head->next == NULL)
+          new = add_dnodeint_end(h, n);
+        else
+        {
+          new = malloc(sizeof(dlistint_t));
+          if (new != NULL)
+          {
+            new->n = n;
+            new->next = head->next;
+            new->prev = head;
+            head->next->prev = new;
+            head->next = new;
+          }
+        }
+        break;
+      }
+      head = head->next;
+      i++;
+    }
+  }
 
-	new = malloc(sizeof(dlistint_t));
-	if new == (NULL);
-		return (NULL);
-	
-	new->n = n;
-	new->prev = temp;
-	new->next = temp->next;
-	temp->next->prev = new;
-	temp->next = new;
-
-	return (new)
+  return (new);
 }
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -6,35 +6,48 @@
  *
  * @head: head of the list
  * @index: index of the node to delete
- * Return: 1 if it succeeded, -1 if it failed
+ * Return: upon success - 1
+ 	* 		on failure - -1
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp = *head;
+  dlistint_t *h1;
+  dlistint_t *h2;
+  unsigned int i;
 
-	if (*head == NULL)
-		return(-1);
-	
-	for (; idx != 0; idx--)
-	{
-		if (temp == NULL)
-                        return (-1);
-		temp = temp->next;
-	}
+  h1 = *head;
 
-	if (temp == *head)
-	{
-		*head = temp->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-	}
+  if (h1 != NULL)
+    while (h1->prev != NULL)
+      h1 = h1->prev;
 
-	else
-	{
-		temp->prev->next = temp->next;
-		if (temp->next !=NULL)
-			temp->next->prev = temp->prev;
-	}
-	free(temp);
-	return(1);
+  i = 0;
+
+  while (h1 != NULL)
+  {
+    if (i == index)
+    {
+      if (i == 0)
+      {
+        *head = h1->next;
+        if (*head != NULL)
+          (*head)->prev = NULL;
+      }
+      else
+      {
+        h2->next = h1->next;
+
+        if (h1->next != NULL)
+          h1->next->prev = h2;
+      }
+
+      free(h1);
+      return (1);
+    }
+    h2 = h1;
+    h1 = h1->next;
+    i++;
+  }
+
+  return (-1);
 }
