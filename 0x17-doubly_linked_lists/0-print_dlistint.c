b3384f36bde2c05ce8ef06a53ad331fae8ee059b
“bahaeeddine10” <bahaeeddine10@gmail.com>
2023-11-10 10:32:42 -0800

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,19 +2,29 @@
 
 /**
  * print_dlistint - prints all the elements of a
- * @h: head of the list
+ * dlistint_t list
  *
+ * @h: head of the list
  * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0
-	
-	while (h)
+	int i;
+
+	i = 0;
+
+	if (h == NULL)
+		return (i);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
+		i++;
 		h = h->next;
-		count++;
 	}
-	return (count);
+
+	return (i);
 }
